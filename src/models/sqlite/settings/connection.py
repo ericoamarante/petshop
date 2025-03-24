5deb15af03f4a64b54cf3139097b6b1fca386ab4
ericoamarante <amarante.erico@gmail.com>
2025-03-23 22:33:31 -0300

    feat: Implementing repository with first test

--- src/models/sqlite/settings/connection.py
+++ src/models/sqlite/settings/connection.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
-class DBConnectionHandler:
+from sqlalchemy.orm import sessionmaker
+
+
+class DBConnectionHanlder:
     def __init__(self) -> None:
         self.__connection_string = "sqlite:///storage.db"
         self.__engine = None
+        self.session = None
 
     def connect_to_db(self):
         self.__engine = create_engine(self.__connection_string)
@@ -10,4 +14,13 @@ class DBConnectionHandler:
     def get_engine(self):
         return self.__engine
 
-db_connection_handler = DBConnectionHandler()
+    def __enter__(self):
+        session_maker = sessionmaker()
+
+        self.session = session_maker(bind=self.__engine)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session.close()
+
+db_connection_handler = DBConnectionHanlder()
