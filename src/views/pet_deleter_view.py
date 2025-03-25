d3fb7e2b3143526a56b6f46642113f9c63ca7663
ericoamarante <amarante.erico@gmail.com>
2025-03-25 16:03:33 -0300

    feat: Implementing project routes for pets and people

--- src/views/pet_deleter_view.py
+++ src/views/pet_deleter_view.py
@@ -1,14 +1,15 @@
-from src.controllers.pet_deleter_controller import PetDeleterController
-from .interfaces.view_interface import ViewInterface
+from src.controllers.interfaces.pet_deleter_controller import PetDeleterControllerInterface
 from .http_types.http_requet import HttpRequest
 from .http_types.http_response import HttpResponse
+from .interfaces.view_interface import ViewInterface
 
-class PersonDeleterView(ViewInterface):
-    def __init__(self, controller: PetDeleterController) -> None:
+class PetDeleterView(ViewInterface):
+    def __init__(self, controller: PetDeleterControllerInterface) -> None:
         self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
         name = http_request.param["name"]
+        print(name)
         self.__controller.delete(name)
 
         return HttpResponse(status_code=204)
